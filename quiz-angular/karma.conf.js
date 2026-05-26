b0557f7d890b50f535c0cb210cab0fb0bb3c87a2
CottageCheese <david@furey.co.uk>
2026-05-26 22:00:35 +0100

    ci(karma): add `--disable-dev-shm-usage` and `--headless` flags to `ChromeHeadlessCI` for improved CI stability

--- quiz-angular/karma.conf.js
+++ quiz-angular/karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--headless']
       }
     },
     restartOnFileChange: true
