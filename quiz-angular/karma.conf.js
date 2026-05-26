3f22615c7c8ae6533b2b2a304650df3818e5d187
CottageCheese <david@furey.co.uk>
2026-05-26 22:15:21 +0100

    ci(karma, workflows): update test execution with `xvfb-run` and new Chrome headless flag for CI stability

--- quiz-angular/karma.conf.js
+++ quiz-angular/karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--headless']
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--headless=new']
       }
     },
     restartOnFileChange: true
